9172b5a895abaa67b47c764bc54c68b2daff70ab
Christopher Imrie <chris@ckidesign.co.uk>
2012-09-13 21:24:41 +0100

    Setting up resources

--- system/expressionengine/third_party/site_manager_client/mcp.site_manager_client.php
+++ system/expressionengine/third_party/site_manager_client/mcp.site_manager_client.php
@@ -18,6 +18,10 @@ class Site_manager_client_mcp
 		if(!property_exists($this->EE, "requirejs")){
 			show_error("The Site Manager module needs the <a href='https://github.com/ckimrie/RequireJS-for-EE'>RequireJS-for-EE</a> extension to be installed in order to function correctly.", 500, "Module Required");
 		}
+
+
+		//What resources do we need?
+		Requirejs::load("css!third_party/site_manager_client/css/site_manager_client.css");
 	}
 
 
@@ -25,6 +29,10 @@ class Site_manager_client_mcp
 	public function index()
 	{
 		$this->EE->cp->set_variable('cp_page_title', "Site Manager");
+		
+
+		
+
 		return $this->EE->load->view("pages/index/index", array(), TRUE);
 	}
 }
